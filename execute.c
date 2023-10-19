57b44cb5b9acd928ace5dd64f965711be251de09
Henshaw <josephhensh2000@gmail.com>
2023-10-19 10:03:59 -0700

    Add file

---
+++ execute.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * execute - executes the opcode
+ *
+ * @stack: pointer to a pointer, pointing to the top of the stack
+ * @line_number: the number of file lines counted,
+ * line number where opcode is found
+ * @content: content of the current file line
+ *
+ * Return: 0 (Success)
+ */
+int execute(stack_t **stack, unsigned int line_number, char *content)
+{
+	unsigned int i = 0;
+	char *op;
+
+	instruction_t op_list[] = {
+		{"push", stack_push},
+		{"pall", struct_pall},
+		{NULL, NULL}
+	};
+
+	op = strtok(content, " \n\t");
+
+	if (op != NULL && op[0] == '#')
+		return (0);
+	instance_vars.monty_arg = strtok(NULL, " \n\t");
+
+	while (op != NULL && op_list[i].opcode)
+	{
+		if (strcmp(op, op_list[i].opcode) == 0)
+		{
+			op_list[i].f(stack, line_number);
+			return (0);
+		}
+		i++;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, op);
+	free_struct(*stack);
+	return (EXIT_FAILURE);
+}
