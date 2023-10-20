fcf02a170178e804c707071622613246d43f4f1e
Henshaw <josephhensh2000@gmail.com>
2023-10-19 20:32:54 -0700

    Update file: add swap_elem function prototype

--- monty.h
+++ monty.h
@@ -64,5 +64,6 @@ void free_struct(stack_t *head);
 void struct_pall(stack_t **top, unsigned int line_number);
 void pint_top(stack_t **stack, unsigned int line_number);
 void pop_top(stack_t **stack, unsigned int line_number);
+void swap_elem(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
